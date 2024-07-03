d4c1e70887483664771fd4b80e005b3a52f1533b
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-07-03 12:18:56 +0700

    7. Melakukan Inisiasi Seeders Untuk Tabel Inti Etalase e-commerce

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            JenisProdukSeeder::class,
+            KategoriTokohSeeder::class,
+            ProdukSeeder::class,
+            TestimoniSeeder::class,
         ]);
     }
 }
