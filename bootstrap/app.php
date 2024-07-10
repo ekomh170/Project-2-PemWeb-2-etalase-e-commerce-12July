00ef37ba8ce84daea50ccf75e4a8eab0a4651f37
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-07-10 11:13:28 +0700

    33. Validasi Role User Admin & Member

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+            'role' => App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\TestimoniController;
 use App\Http\Controllers\JenisProdukController;
 use App\Http\Controllers\KategoriTokohController;
+use App\Http\Middleware\RoleMiddleware;
 // use App\Http\Controllers\AccountUserController;
 
 use Illuminate\Support\Facades\Route;
@@ -65,60 +66,63 @@ Route::middleware('auth')->group(function () {
 // Profile
 
 
-
 // Master Data Utama
 // Produk
-Route::get('/panel/produk', [ProdukController::class, 'index'])->name('produk.index');
-Route::get('/panel/produk/data', [ProdukController::class, 'getData'])->name('produk.data');
-Route::get('/panel/produk/create', [ProdukController::class, 'create'])->name('produk.create');
-Route::post('/panel/produk/store', [ProdukController::class, 'store'])->name('produk.store');
-Route::get('/panel/produk/{produk}', [ProdukController::class, 'show'])->name('produk.show');
-Route::get('/panel/produk/{produk}/edit', [ProdukController::class, 'edit'])->name('produk.edit');
-Route::put('/panel/produk/{produk}', [ProdukController::class, 'update'])->name('produk.update');
-Route::delete('/panel/produk/{produk}', [ProdukController::class, 'destroy'])->name('produk.destroy');
-// Produk
 
-// Testimoni
-Route::get('/panel/testimoni', [TestimoniController::class, 'index'])->name('testimoni.index');
-Route::get('/panel/testimoni/create', [TestimoniController::class, 'create'])->name('testimoni.create');
-Route::post('/panel/testimoni/store', [TestimoniController::class, 'store'])->name('testimoni.store');
-Route::get('/panel/testimoni/{testimoni}', [TestimoniController::class, 'show'])->name('testimoni.show');
-Route::get('/panel/testimoni/{testimoni}/edit', [TestimoniController::class, 'edit'])->name('testimoni.edit');
-Route::put('/panel/testimoni/{testimoni}', [TestimoniController::class, 'update'])->name('testimoni.update');
-Route::delete('/panel/testimoni/{testimoni}', [TestimoniController::class, 'destroy'])->name('dokter.destroy');
-// Testimoni
-// Master Data Utama
+Route::middleware(['auth', RoleMiddleware::class . ':admin'])->group(function () {
+    Route::get('/panel/produk', [ProdukController::class, 'index'])->name('produk.index');
+    Route::get('/panel/produk/data', [ProdukController::class, 'getData'])->name('produk.data');
+    Route::get('/panel/produk/create', [ProdukController::class, 'create'])->name('produk.create');
+    Route::post('/panel/produk/store', [ProdukController::class, 'store'])->name('produk.store');
+    Route::get('/panel/produk/{produk}', [ProdukController::class, 'show'])->name('produk.show');
+    Route::get('/panel/produk/{produk}/edit', [ProdukController::class, 'edit'])->name('produk.edit');
+    Route::put('/panel/produk/{produk}', [ProdukController::class, 'update'])->name('produk.update');
+    Route::delete('/panel/produk/{produk}', [ProdukController::class, 'destroy'])->name('produk.destroy');
+    // Produk
 
-
-
-// Master Pengelompokan
-// Jenis Produk
-Route::get('/panel/jenisProduk', [JenisProdukController::class, 'index'])->name('jenisProduk.index');
-Route::get('/panel/jenisProduk/create', [JenisProdukController::class, 'create'])->name('jenisProduk.create');
-Route::post('/panel/jenisProduk/store', [JenisProdukController::class, 'store'])->name('jenisProduk.store');
-Route::get('/panel/jenisProduk/{jenisProduk}', [JenisProdukController::class, 'show'])->name('jenisProduk.show');
-Route::get('/panel/jenisProduk/{jenisProduk}/edit', [JenisProdukController::class, 'edit'])->name('jenisProduk.edit');
-Route::put('/panel/jenisProduk/{jenisProduk}', [JenisProdukController::class, 'update'])->name('jenisProduk.update');
-Route::delete('/panel/jenisProduk/{jenisProduk}', [JenisProdukController::class, 'destroy'])->name('jenisProduk.destroy');
-// Jenis Produk
-
-// Kategori Tokoh
-Route::get('/admin/kategoriTokoh', [KategoriTokohController::class, 'index'])->name('kategoriTokoh.index');
-Route::get('/admin/kategoriTokoh/create', [KategoriTokohController::class, 'create'])->name('kategoriTokoh.create');
-Route::post('/admin/kategoriTokoh/store', [KategoriTokohController::class, 'store'])->name('kategoriTokoh.store');
-Route::get('/admin/kategoriTokoh/{kategoriTokoh}', [KategoriTokohController::class, 'show'])->name('kategoriTokoh.show');
-Route::get('/admin/kategoriTokoh/{kategoriTokoh}/edit', [KategoriTokohController::class, 'edit'])->name('kategoriTokoh.edit');
-Route::put('/admin/kategoriTokoh/{kategoriTokoh}', [KategoriTokohController::class, 'update'])->name('kategoriTokoh.update');
-Route::delete('/admin/kategoriTokoh/{kategoriTokoh}', [KategoriTokohController::class, 'destroy'])->name('kategoriTokoh.destroy');
-// Kategori Tokoh
-// Master Pengelompokan
+    // Testimoni
+    Route::get('/panel/testimoni', [TestimoniController::class, 'index'])->name('testimoni.index');
+    Route::get('/panel/testimoni/data', [TestimoniController::class, 'getData'])->name('testimoni.data');
+    Route::get('/panel/testimoni/create', [TestimoniController::class, 'create'])->name('testimoni.create');
+    Route::post('/panel/testimoni/store', [TestimoniController::class, 'store'])->name('testimoni.store');
+    Route::get('/panel/testimoni/{testimoni}', [TestimoniController::class, 'show'])->name('testimoni.show');
+    Route::get('/panel/testimoni/{testimoni}/edit', [TestimoniController::class, 'edit'])->name('testimoni.edit');
+    Route::put('/panel/testimoni/{testimoni}', [TestimoniController::class, 'update'])->name('testimoni.update');
+    Route::delete('/panel/testimoni/{testimoni}', [TestimoniController::class, 'destroy'])->name('testimoni.destroy');
+    // Testimoni
+    // Master Data Utama
 
 
-// Konfigurasi
-// Account Users
 
-// Account Users
-// Konfigurasi
+    // Master Pengelompokan
+    // Jenis Produk
+    Route::get('/panel/jenisProduk', [JenisProdukController::class, 'index'])->name('jenisProduk.index');
+    Route::get('/panel/jenisProduk/create', [JenisProdukController::class, 'create'])->name('jenisProduk.create');
+    Route::post('/panel/jenisProduk/store', [JenisProdukController::class, 'store'])->name('jenisProduk.store');
+    Route::get('/panel/jenisProduk/{jenisProduk}', [JenisProdukController::class, 'show'])->name('jenisProduk.show');
+    Route::get('/panel/jenisProduk/{jenisProduk}/edit', [JenisProdukController::class, 'edit'])->name('jenisProduk.edit');
+    Route::put('/panel/jenisProduk/{jenisProduk}', [JenisProdukController::class, 'update'])->name('jenisProduk.update');
+    Route::delete('/panel/jenisProduk/{jenisProduk}', [JenisProdukController::class, 'destroy'])->name('jenisProduk.destroy');
+    // Jenis Produk
+
+    // Kategori Tokoh
+    Route::get('/admin/kategoriTokoh', [KategoriTokohController::class, 'index'])->name('kategoriTokoh.index');
+    Route::get('/admin/kategoriTokoh/create', [KategoriTokohController::class, 'create'])->name('kategoriTokoh.create');
+    Route::post('/admin/kategoriTokoh/store', [KategoriTokohController::class, 'store'])->name('kategoriTokoh.store');
+    Route::get('/admin/kategoriTokoh/{kategoriTokoh}', [KategoriTokohController::class, 'show'])->name('kategoriTokoh.show');
+    Route::get('/admin/kategoriTokoh/{kategoriTokoh}/edit', [KategoriTokohController::class, 'edit'])->name('kategoriTokoh.edit');
+    Route::put('/admin/kategoriTokoh/{kategoriTokoh}', [KategoriTokohController::class, 'update'])->name('kategoriTokoh.update');
+    Route::delete('/admin/kategoriTokoh/{kategoriTokoh}', [KategoriTokohController::class, 'destroy'])->name('kategoriTokoh.destroy');
+    // Kategori Tokoh
+    // Master Pengelompokan
+
+
+    // Konfigurasi
+    // Account Users
+
+    // Account Users
+    // Konfigurasi
+});
 
 // Administrator
 
